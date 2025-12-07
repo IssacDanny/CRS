181b0d8ff1577e0986b9f26cdb2ef9b5b52ace99
cuongchuck <cuong.buiducmanh@gmail.com>
2025-12-07 23:58:03 +0700

    change username to email, add get user info

--- services/backend-app/src/controllers/auth.controller.js
+++ services/backend-app/src/controllers/auth.controller.js
@@ -3,30 +3,30 @@ const authService = require('../services/auth.service');
 
 const register = async (req, res) => {
   try {
-    const { username, password, fullName, role } = req.body;
+    const { email, password, fullName, role } = req.body;
 
     // Basic validation
-    if (!username || !password || !fullName || !role) {
+    if (!email || !password || !fullName || !role) {
       return res.status(400).json({ message: 'All fields are required' });
     }
 
-    const newUser = await authService.registerUser(username, password, fullName, role);
+    const newUser = await authService.registerUser(email, password, fullName, role);
     res.status(201).json(newUser);
   } catch (error) {
-    // Send a specific error if the username is taken
+    // Send a specific error if the email is taken
     res.status(409).json({ message: error.message }); // 409 Conflict
   }
 };
 
 const login = async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { email, password } = req.body;
 
-    if (!username || !password) {
-      return res.status(400).json({ message: 'Username and password are required' });
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Email and password are required' });
     }
 
-    const result = await authService.loginUser(username, password);
+    const result = await authService.loginUser(email, password);
     res.status(200).json(result);
   } catch (error) {
     // Send a generic 401 Unauthorized for login failures
@@ -34,7 +34,21 @@ const login = async (req, res) => {
   }
 };
 
+const getUser = async (req, res) => {
+  try {
+    const token = req.headers['token'];
+    if (!token) {
+      return res.status(400).json({ message: 'JWT token is required' });
+    }
+    const result = await authService.getUser(token);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
 module.exports = {
   register,
   login,
+  getUser
 };
